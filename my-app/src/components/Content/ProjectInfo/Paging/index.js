d19b947229bc44b3db442ff6f77c73cea988ba7e
daisy.liao <daisy.liao@silksoftware.com>
2023-04-17 11:04:44 +0800

    4.17代码提交

---
+++ my-app/src/components/Content/ProjectInfo/Paging/index.js
@@ -0,0 +1,52 @@
+import React ,{useState,useEffect}from 'react'
+
+import pagingstyle from './index.module.css' 
+
+export default function Paging(props) {
+  // 获取的所有数据
+  let userProj = props.userProj[0]
+  // 分页数组容器
+  let [pagecontainer,setpagecontainer] = props.pagecontainer
+
+  // 初始化页数
+  let [num,setnum] = useState(1)
+  // 初始化最大页数
+  let [maxnum,setmaxnum] = useState(0)
+  // 初始化每页的元素数量
+  let everynum = useState(5)[0]
+
+  // 初次渲染时，计算最大的页数赋给最大页数
+  useEffect(()=>{
+    setmaxnum(Math.ceil(userProj.length/everynum))
+  },[userProj])
+
+  // 分页函数处理，更新分页数组的内容
+  let fenye =(userProj)=>{
+    setpagecontainer(userProj.slice((num-1)*everynum,(num-1)*everynum+everynum))
+  }
+  useEffect(()=>{
+    console.log('pagecontainer为',pagecontainer)
+  },[pagecontainer])
+
+  // 页数改变后，执行分页函数
+  useEffect(()=>{
+    fenye(userProj)
+  },[num,userProj])
+
+  // 上一页函数
+  let dele = ()=>{
+    setnum(num-1)
+  }
+  // 下一页函数
+  let add =()=>{
+    setnum(num+1)
+  }
+  
+  return (
+    <div className={pagingstyle.out}>
+      {num>1&&<div onClick={dele}>上一页</div>}
+      {userProj&&<div>{num}/{maxnum}</div>}
+      {num<maxnum&&<div onClick={add}>下一页</div>}  
+    </div>
+  )
+}
--- my-app/src/components/Content/ProjectInfo/index.js
+++ my-app/src/components/Content/ProjectInfo/index.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import {Link} from 'react-router-dom'
 
 import dayjs from 'dayjs'
-
 import ProjectStyle from './index.module.css'
 
+import Paging from './Paging'
+
 export default function ProjectInfo(props) {
-  // 获取用户信息
-  const userProj = Array.from(props.userProj[0])
+  // 获取用户所有项目信息
+  const [userProj,setuserProj] = props.userProj
+
+  // 分页数组容器
+  const [pagecontainer,setpagecontainer] = useState('')
+
+  useEffect(()=>{
+    console.log('分页结果',pagecontainer)
+  },[pagecontainer])
   
   return (
     <div className={ProjectStyle.out}>
+      <Paging 
+        userProj={[userProj,setuserProj]}
+        pagecontainer={[pagecontainer,setpagecontainer]}
+      ></Paging>
       <div className={ProjectStyle.topLine}></div>
-      {userProj.map((item)=>{
+      {pagecontainer&&pagecontainer.map((item)=>{
         return(
           <div key={item.id} className={ProjectStyle.proj}>
             <Link to={`/detail/${item.owner.login}/${item.name}`}>
