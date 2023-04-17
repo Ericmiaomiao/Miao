3dcfa127c2d7a62076f5478c59c93ef3630a2ad5
daisy.liao <daisy.liao@silksoftware.com>
2023-04-17 14:36:00 +0800

    4.17代码第四次修改

--- my-app/src/pages/Login/index.js
+++ my-app/src/pages/Login/index.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import {useNavigate} from 'react-router-dom'
+
+import loginStyle from './index.module.css'
 
 export default function Login() {
+  let navigate = useNavigate()
+  let usernameref = useRef(null)
+  let passwordref = useRef(null)
+  let sign =()=>{
+    if(!usernameref.current.value){
+      alert('请输入用户名')
+      return
+    }
+    navigate({pathname:'/user'},{state:{username:usernameref.current.value}})
+  }
   return (
-    <div>Login</div>
+    <div className={loginStyle.out}>
+      <div className={loginStyle.loginOut}>
+        <div className={loginStyle.logo}>
+          <i className={loginStyle.iconlogo}>&#xe644;</i>
+        </div>
+        <div>登录 Github Viewer</div>
+        <div className={loginStyle.inputOut}>
+          <div className={loginStyle.text}>用户名</div>
+          <div className={loginStyle.input}>
+            <input ref={usernameref}/>
+          </div>
+          <div className={loginStyle.text}>密码</div>
+          <div className={loginStyle.input}>
+            <input ref={passwordref}/>
+          </div>
+          <div className={loginStyle.loginButton} onClick={sign}>Sign in</div>
+        </div>
+
+      </div>
+    </div>
   )
 }
